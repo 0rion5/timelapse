b43f62a68e4a1c45e791bc84606e6b972ee5e861
0rion <45885529+0rion5@users.noreply.github.com>
2020-12-14 13:55:05 -0700

    Update webcam.sh

--- webcam.sh
+++ webcam.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Change directory to working folder
-cd ~/timelapse
-
-# YOUR USERNAME
-USERNAME=$"YOURUSERNAME"
-
 # directory to save the picture
-SAVE_DIR=$"/home/$USERNAME/Pictures/"
+SAVE_DIR=$HOME$"/Pictures/"
 
 # name of the file (Just the Date)
 FILE_NAME=$(date +"%F_%H-%M")
 
-# Configuration file location
-CONFIG=$'/home/$USERNAME/timelapse/config.conf'
+# save file
+SAVE=$SAVE_DIR$FILE_NAME.jpg
 
-# take a picture
-fswebcam -c $CONFIG $SAVE_DIR$DATE.jpg
+# Configuration file location
+CONFIG=$HOME$'/timelapse/config.conf'
+
+#take a picture
+fswebcam -c$CONFIG $SAVE.jpg
