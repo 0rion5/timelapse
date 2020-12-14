748e053f7db49273c9313c7893dfa7eaeb3d5a50
0rion <45885529+0rion5@users.noreply.github.com>
2020-12-14 13:38:10 -0700

    Update webcam.sh
    changed some things so its easier to configure. aka less hard code

--- webcam.sh
+++ webcam.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-DATE=$(date +"%F_%H-%M")
+# Change directory to working folder
+cd ~/timelapse
 
-fswebcam -r 1280x720 --no-banner /media/pi/webcam/stills/$DATE.jpg
+# YOUR USERNAME
+USERNAME=$"YOURUSERNAME"
+
+# directory to save the picture
+SAVE_DIR=$"/home/$USERNAME/Pictures/"
+
+# name of the file (Just the Date)
+FILE_NAME=$(date +"%F_%H-%M")
+
+# Configuration file location
+CONFIG=$'/home/$USERNAME/timelapse/config.conf'
+
+# take a picture
+fswebcam -c $CONFIG $SAVE_DIR$DATE.jpg
